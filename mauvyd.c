0f044ee5145c02e7ad34b5a39a68caad0fbcc8bc
Mehmet Demir <mehmetd@vuhuv.com>
2026-05-29 19:57:15 +0300

    Added args parser and added details
    Removed hardcoded paths and defined as a variable

--- mauvyd.c
+++ mauvyd.c
@@ -13,28 +13,109 @@
 #include <errno.h>
 #include "mauvyd-headers/pcg.h"
 
+#define CONFIG_DIR "/etc/mauvyd"
+#define DEFAULT_PATH "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+#define HOSTNAME "local@tuxshome" // <-- you can change this
+#define OS_NAME "An Linux Distro" // <-- you can change this too
+
+void setup_network() {
+    struct ifreq ifr;
+    int fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (fd < 0) { perror("socket"); return; }
+
+    memset(&ifr, 0, sizeof(ifr));
+    strncpy(ifr.ifr_name, "lo", IFNAMSIZ);
+    ioctl(fd, SIOCGIFFLAGS, &ifr);
+    ifr.ifr_flags |= IFF_UP | IFF_RUNNING;
+    ioctl(fd, SIOCSIFFLAGS, &ifr);
+
+    memset(&ifr, 0, sizeof(ifr));
+    strncpy(ifr.ifr_name, "ens3", IFNAMSIZ);
+    ioctl(fd, SIOCGIFFLAGS, &ifr);
+    ifr.ifr_flags |= IFF_UP | IFF_RUNNING;
+    ioctl(fd, SIOCSIFFLAGS, &ifr);
+
+    close(fd);
+    printf("[+]: Network interfaces activated.\n");
+}
+
+void start_services(struct dirent **namelist, int n) {
+    for (int i = 0; i < n; i++) {
+        struct dirent *entry = namelist[i];
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+            free(namelist[i]);
+            continue;
+        }
+
+        char fulldst[512];
+        snprintf(fulldst, sizeof(fulldst), "%s/%s", CONFIG_DIR, entry->d_name);
+
+        char location[256] = {0};
+        char wait_str[16] = {0};
+        char args_str[512] = {0};
+
+        pcg_read(fulldst, "location", location, sizeof(location));
+        pcg_read(fulldst, "wait", wait_str, sizeof(wait_str));
+        pcg_read(fulldst, "args", args_str, sizeof(args_str));
+
+        if (location[0] != '/') {
+            printf("[!!!] Invalid location: %s\n", entry->d_name);
+            free(namelist[i]);
+            continue;
+        }
+
+        printf("Found Process: %s\n", entry->d_name);
+
+        pid_t pid = fork();
+        if (pid == 0) {
+            printf("Child Process starting this service: %s\n", location);
+
+            char *argv[32] = {0};
+            argv[0] = location;
+
+            if (args_str[0] != '\0') {
+                char *token = strtok(args_str, " ");
+                int j = 1;
+                while (token && j < 31) {
+                    argv[j++] = token;
+                    token = strtok(NULL, " ");
+                }
+            }
+
+            execv(location, argv);
+            perror("execv failed");
+            exit(1);
+        } else if (pid > 0 && strcmp(wait_str, "1") == 0) {
+            waitpid(pid, NULL, 0);
+        }
+
+        free(namelist[i]);
+    }
+}
+
 int main() {
-    struct dirent *entry;
     pid_t pid;
     struct dirent **namelist;
-    int n = scandir("path/to/config/folder", &namelist, NULL, alphasort);
+    int n = scandir(CONFIG_DIR, &namelist, NULL, alphasort);
     if (n < 0) {
         perror("[!]: Can't open the config folder, Continue to boot..");
-        char *shell_args[] = {"/path/to/shell", NULL};
-        execv("/bin/shell", shell_args);
+        char *shell_args[] = {"/bin/sh", NULL};
+        execv("/bin/sh", shell_args);
         while(wait(NULL) > 0);
         return 0;
     }
+
     printf("----------------------------------------\n");
     printf("----- MAUVYD Configuration System -----\n");
     printf("-------------Mounting FS..--------------\n");
-    mount("proc", "/proc", "proc", 0, NULL); // Start of the Mount and the Persistence settings
-    mount("sysfs", "/sys", "sysfs", 0, NULL); // You can change these how as you want it! :)
+
+    mount("proc", "/proc", "proc", 0, NULL);
+    mount("sysfs", "/sys", "sysfs", 0, NULL);
     mount("devtmpfs", "/dev", "devtmpfs", 0, NULL);
     mount("tmpfs", "/tmp", "tmpfs", 0, NULL);
     mkdir("/data", 0755);
-    mkdir("/data/...", 0755); // <-- change here (example: /data/<yourcommandfolder)
-    int ret = mount("/dev/vda", "/data", "ext4", 0, NULL); // <-- you can add more mount points and device nodes much as you want.
+
+    int ret = mount("/dev/vda", "/data", "ext4", 0, NULL); // <-- you can add more mount points and device nodes as you want
     if (ret == 0) {
         printf("[+]: Persistence is active.\n");
     } else if (errno == EBUSY) {
@@ -42,80 +123,18 @@ int main() {
     } else {
         perror("[!]: Can't connect the disk (persistence)\n");
     }
+
     printf("[!] Setting up the hostname..\n");
-    sethostname("hostname_here", strlen("hostname_here")); // <-- change here
-    putenv("PATH=path/to/commands/folder");
-    putenv("TERM=linux");
-    printf("Your OS Name here (f.e PatiOS by PatiOS Team)\n");
+    sethostname(HOSTNAME, strlen(HOSTNAME));
+    setenv("PATH", DEFAULT_PATH, 1);
+    setenv("TERM", "linux", 1);
+    printf("%s\n", OS_NAME);
 
-    struct ifreq ifr;
-    int tmp_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    signal(SIGCHLD, SIG_IGN);
+    setup_network();
+    start_services(namelist, n);
+    free(namelist);
 
-    memset(&ifr, 0, sizeof(ifr));
-    strncpy(ifr.ifr_name, "lo", IFNAMSIZ);
-    ioctl(tmp_fd, SIOCGIFFLAGS, &ifr);
-    ifr.ifr_flags |= IFF_UP | IFF_RUNNING;
-    ioctl(tmp_fd, SIOCSIFFLAGS, &ifr);
-
-    memset(&ifr, 0, sizeof(ifr));
-    strncpy(ifr.ifr_name, "eth0", IFNAMSIZ);
-    ioctl(tmp_fd, SIOCGIFFLAGS, &ifr);
-    ifr.ifr_flags |= IFF_UP | IFF_RUNNING;
-    ioctl(tmp_fd, SIOCSIFFLAGS, &ifr);
-
-    close(tmp_fd);
-
-for (int i = 0; i < n; i++) {
-    entry = namelist[i];
-    if (strcmp(entry->d_name, ".") == 0) {
-        continue;
-    };
-    if (strcmp(entry->d_name, "..") == 0) {
-        continue;
-    };
-      usleep(10000);
-      printf("Found Process: %s\n", entry->d_name);
-      char fulldst[512];
-      snprintf(fulldst, sizeof(fulldst), "/path/to/config/folder/%s", entry->d_name); // <-- Change here
-    char filedst[256] = {0};
-    char wait_val[16] = {0};
-    char watch_val[16] = {0};
-    pcg_read(fulldst, "location", filedst, sizeof(filedst));
-    if (filedst[0] != '/' || strstr(filedst, "..") != NULL) {
-        printf("[!!!] Skipping this, wrong folder location: %s\n", filedst);
-        free(namelist[i]);
-        continue;
-    }
-    pcg_read(fulldst, "wait", wait_val, sizeof(wait_val));
-    pcg_read(fulldst, "watch", watch_val, sizeof(watch_val));
-    char *args[] = {NULL, NULL};
-    args[0] = filedst;
-    int wait_flag = (strcmp(wait_val, "1") == 0);
-    int watch_flag = (strcmp(watch_val, "1") == 0);
-
-
-      pid = fork(); // FORK TIME!
-
-    if (pid == -1) {
-        perror("Fork failed.");
-        exit(EXIT_FAILURE);
-        }
-    if (pid > 0 && wait_flag == 1) {
-        usleep(100000);
-        waitpid(pid, NULL, 0);
-        }
-
-    if (pid == 0) {
-        printf("Child Process starting this service: %s\n", filedst);
-        execv(filedst, args);
-        perror("Oops, process got sick! (cant run this service)");
-        exit(EXIT_FAILURE);
-        }
-free(namelist[i]);
-}
-free(namelist);
-signal(SIGCHLD, SIG_IGN);
-sigset_t mask;
-sigemptyset(&mask);
-sigsuspend(&mask);
+    while(1) pause();
+    return 0;
 }
