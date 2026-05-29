dc8a545c675caff94039490ef92218b89f5f57c8
Mehmet Demir <mehmetd@vuhuv.com>
2026-05-29 15:20:48 +0300

    Refactor pcg_read function variable names

--- mauvyd-headers/pcg.c
+++ mauvyd-headers/pcg.c
@@ -1,12 +1,12 @@
 #include "pcg.h"
 
-int pcg_read(const char *dosya_yolu, const char *key, char *buf, size_t buf_sz) {
-    FILE *f = fopen(dosya_yolu, "r");
+int pcg_read(const char *file_dst, const char *key, char *buf, size_t buf_sz) {
+    FILE *f = fopen(file_dst, "r");
     if (!f) return -1;
 
-    char satir[512];
-    while (fgets(satir, sizeof(satir), f)) {
-        char *k = strtok(satir, " =\n");
+    char line[512];
+    while (fgets(line, sizeof(line), f)) {
+        char *k = strtok(line, " =\n");
         if (!k) continue;
         if (strcmp(k, key) == 0) {
             char *v = strtok(NULL, " =\n");
